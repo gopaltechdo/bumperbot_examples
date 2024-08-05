f32a16714ec654f41941eb2367f94bf6123ffafc
gopal kumar <gopalsingh.g60@gmail.com>
2024-08-05 17:41:51 +0530

    urdf

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gopal/ros/bumperbot_ws/src/bumperbot_examples
-# Build directory: /home/gopal/ros/bumperbot_ws/src/bumperbot_examples/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
